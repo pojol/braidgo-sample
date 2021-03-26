7233ec0b4a6ad3858e8e944e94f9d8c752d0964f
pojol <pojol@outlook.com>
2021-03-26 19:29:57 +0800

    # up

--- build/gate_run.sh
+++
@@ -1,6 +0,0 @@
-#! bin/bash
-
-docker run -d -p 14001:14001/tcp braid-sample/gateway:latest \
-    -consul http://172.17.0.1:8500 \
-    -nsqlookup 172.17.0.1:4161 \
-    -nsqd 172.17.0.1:4150 \
--- build/login_run.sh
+++
@@ -1,6 +0,0 @@
-#! bin/bash
-
-docker run -d  -p 14101:14101/tcp braid-sample/login:latest \
-    -consul http://172.17.0.1:8500 \
-    -nsqlookup 172.17.0.1:4161 \
-    -nsqd 172.17.0.1:4150 \
